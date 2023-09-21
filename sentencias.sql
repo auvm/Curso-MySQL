491df63be5940e5014fbff2a7ccb7e60d6a81106
auvm <angelurielvelascom@gmail.com>
2023-09-20 22:36:41 -0600

    add unsigned numbers and primary key definitions

--- sentencias.sql
+++ sentencias.sql
@@ -5,36 +5,24 @@ CREATE DATABASE IF NOT EXISTS libreria_cf CHARACTER SET "UTF8";
 USE libreria_cf;
 
 CREATE TABLE IF NOT EXISTS autores( 
-autor_id INT NOT NULL,
+autor_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 nombre VARCHAR(50) NOT NULL,
 apellido VARCHAR(50) NOT NULL,
 seudonimo VARCHAR(50) UNIQUE, 
-genero CHAR(1) NOT NULL, -- M o F
+genero ENUM("M", "F") NOT NULL, -- M o F
 fecha_nacimiento DATE NOT NULL, -- Mes-Año-Dia
 pais_origen VARCHAR(40) NOT NULL,
 fecha_de_creacion DATETIME DEFAULT NOW() 
 );
 
-INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen) 
-VALUES (1, "Test autor", "Test autor", "M", "2000-06-13", "México");
-
-INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen) 
-VALUES (2, "Test autor", "Test autor", "M", "2000-06-13", "México");
-
-INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen) 
-VALUES (3, "Test tercero", "Test tercero", "M", "2018-2-10", "México");
-
-INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen) 
-VALUES (4, "Test cuarto", "Test cuarto", "F", "2017-09-19", "Japón");
-
-INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen) 
-VALUES (5, "Test quinto", "Test quinto", "M", "2005-04-20", "México");
-
-INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen) 
-VALUES (6, "Test sexto", "Test sexto", "F", "2005-07-18", "Alemania");
-
-INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen) 
-VALUES (7, "Test séptimo", "Test séptimo", "F", "2010-12-24", "Rusia");
+INSERT INTO autores (nombre, apellido, genero, fecha_nacimiento, pais_origen) 
+VALUES ("Test autor", "Test autor", "M", "2000-06-13", "México"),
+("Test autor", "Test autor", "M", "2000-06-13", "México"),
+("Test tercero", "Test tercero", "M", "2018-2-10", "México"),
+("Test cuarto", "Test cuarto", "F", "2017-09-19", "Japón"),
+("Test quinto", "Test quinto", "M", "2005-04-20", "México"),
+("Test sexto", "Test sexto", "F", "2005-07-18", "Alemania"),
+("Test séptimo", "Test séptimo", "F", "2010-12-24", "Rusia");
 
 
 INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen) 
